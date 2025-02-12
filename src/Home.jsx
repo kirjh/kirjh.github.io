ab3cdfddc7e5e9d1fe920d1f6c3f9d0f39da8cc6
kirjh <71944013+kirjh@users.noreply.github.com>
2025-02-11 20:02:51 -0800

    Enable smooth scrolling when clicking on navigation links

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,9 @@ export default function Home() {
     <>
       <div id="home" className="pagecontainer">
         <p>this is a paragraph</p>
+        <a href="#about">click me</a><br/>
+        <a href="#projects">click me</a><br/>
+        <a href="#contact">click me</a><br/>
       </div>
     </>
   );
