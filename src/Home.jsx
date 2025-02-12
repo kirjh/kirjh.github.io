d12602c899a345fffd4c4eb49a4cd112e84b081d
kirjh <71944013+kirjh@users.noreply.github.com>
2025-02-12 01:14:57 -0800

    Add layout for title and links to other pages

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,8 +8,21 @@ export default function Home() {
           <div className="image container">
             <img src="src/assets/profile_picture.png" alt="Painting of a person wearing a rabbit mask" />
           </div>
-          <div className="title container">
-
+          <div className="page container">
+            <div className="title">
+              <p>kirjh.github.io</p>
+            </div>
+            <div className="pagelinks">
+              <span>
+                <a href="#about">About</a>
+              </span>
+              <span>
+                <a href="#projects">Projects</a>
+              </span>
+              <span>
+                <a href="#contact">Contact</a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
