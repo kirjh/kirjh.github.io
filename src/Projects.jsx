61ef966e27ef492d698a741a22df2bce6361a923
kirjh <71944013+kirjh@users.noreply.github.com>
2025-02-13 01:04:07 -0800

    Add ability to parse json files

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,11 +1,26 @@
 import './projects.css'
+import './index.css'
+
+import galatimer from './galatimer.json'
 
 export default function Projects() {
   return (
     <>
       <div id="projects" className="pagecontainer">
-        <p>this is a paragraph</p>
+        <div className="flexbox"> 
+          <Project project={galatimer}/>
+        </div>
       </div>
     </>
   );
+}
+
+function Project({ project }) {
+  return (
+    <>
+      <div className="projectspage container">
+        <p>{project.name}</p>
+      </div>
+    </>
+  )
 }
