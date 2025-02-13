2f6cec7a9ffcd48ecfc1b8d02cfa4a26f6e8d708
kirjh <71944013+kirjh@users.noreply.github.com>
2025-02-13 12:21:45 -0800

    Set up sections of projects page

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -19,13 +19,20 @@ function Project({ project }) {
   return (
     <>
       <div className="projectspage container">
-          <div className="padder">
+        <div className="sectiondivider">
+          <div className="section">
             <div className="header">
               <h1>{project.h1}</h1>
               <h2>{project.h2}</h2>
             </div>
+            <p>{project.name}</p>
           </div>
-        <p>{project.name}</p>
+          <div className="section">
+            <p>test</p>
+          </div>
+        </div>
+        
+          
       </div>
     </>
   )
