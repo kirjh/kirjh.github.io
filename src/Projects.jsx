7d4caebf1b6561d35836c672e79274b08b1a3c4e
kirjh <71944013+kirjh@users.noreply.github.com>
2025-02-13 01:05:50 -0800

    Add Title to project screen

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -19,6 +19,12 @@ function Project({ project }) {
   return (
     <>
       <div className="projectspage container">
+          <div className="padder">
+            <div className="header">
+              <h1>{project.h1}</h1>
+              <h2>{project.h2}</h2>
+            </div>
+          </div>
         <p>{project.name}</p>
       </div>
     </>
