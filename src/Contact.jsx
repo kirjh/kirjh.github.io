e099c8f668f233d65613e1f26a1a6885c30ebd18
kirjh <71944013+kirjh@users.noreply.github.com>
2025-02-12 21:07:21 -0800

    Setup contacts page with title and boxes for contact info

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,11 +1,44 @@
 import './contact.css'
+import './index.css'
+import './about.css'
 
 export default function Contact() {
   return (
     <>
       <div id="contact" className="pagecontainer">
-        <p>this is a paragraph</p>
+        <div className="flexbox"> 
+          <div className="contactpage container">
+            <div className="padder">
+              <div className="header">
+                <h1>Let's</h1>
+                <h2>Get down to business</h2>
+              </div>
+            </div>
+            <div className="sectiondivider">
+              <ContactBox />
+              <ContactBox />
+              <ContactBox />
+              <ContactBox />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
+}
+
+function ContactBox() {
+  return (
+    <>
+      <div className="contactbox">
+        <h3>
+          Title
+        </h3>
+        <p>
+          Link here
+        </p>
+
+      </div>
+    </>
+  )
 }
