888abdd2b01ed87dffba54a02f0a617197c2ef03
kirjh <71944013+kirjh@users.noreply.github.com>
2025-10-30 23:33:22 -0700

    make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,26 @@ import '../styles/navbar.css'
 function Navbar() {
   function goto(href) {
     document.querySelector(href).scrollIntoView();
+    document.querySelector("#menu").classList.remove("show");
+    document.querySelector("button").innerHTML = "Menu";
+  }
+
+  function openMenu() {
+    const menu = document.querySelector("#menu");
+    menu.classList.toggle("show");
+    document.querySelector("button").innerHTML = menu.classList.contains("show") ? "Close" : "Menu";
   }
   
   return (
     <>
       <div className="navbar">
-        <a onClick={()=> goto("#home")}>Home</a>
-        <a onClick={()=> goto("#about")}>About</a>
-        <a onClick={()=> goto("#projects")}>Projects</a>
-        <a onClick={()=> goto("#contact")}>Contact</a>
+        <button onClick={()=> openMenu()}>Menu</button>
+        <div id="menu">
+          <a onClick={()=> goto("#home")}>Home</a>
+          <a onClick={()=> goto("#about")}>About</a>
+          <a onClick={()=> goto("#projects")}>Projects</a>
+          <a onClick={()=> goto("#contact")}>Contact</a>
+        </div>
       </div>
       <div className="navbar-spacer"></div>
     </>
