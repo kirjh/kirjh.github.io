f891775e937de2a2fdb9eb297618c53c4aa94cc2
kirjh <71944013+kirjh@users.noreply.github.com>
2025-12-13 13:41:51 -0800

    Refactor projects cards and style scroll buttons

---
+++ src/components/Card.jsx
@@ -0,0 +1,118 @@
+import '../styles/projects.css'
+import '../styles/card.css'
+import '../styles/index.css'
+
+import Heading from './Heading.jsx'
+
+import {useEffect} from 'react'
+
+function Arrow() {
+  return (
+    <>
+      <svg className="arrow"
+        viewBox="0 0 1.2001499 1.2001499"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:svg="http://www.w3.org/2000/svg">
+        <defs
+          id="defs1" />
+        <path
+          style={{'fill':'none','stroke':'currentColor','strokeWidth':'0.264583','strokeLinecap':'round','strokeLinejoin':'round','strokeDasharray':'none'}}
+          d="m 0.13235301,0.73432379 0.4677219,-0.4677219 0.46772199,0.4677219"
+          id="path1" />
+      </svg>
+    </>
+  )
+}
+
+function CarouselImage({index, id, src, alt}) {
+  return (
+    <li className="image" id={id} index={index}>
+      <img src={src} alt={alt} />
+    </li>
+  )  
+}
+function ProjectLinks({site, repo}) {
+  return site ? (
+    <span className="card-link-container">
+      <a className="link" href={site} target="_blank">Website</a>
+      <span className="subtext" aria-hidden="true">|</span>
+      <a className="link" href={repo} target="_blank">Github page</a>
+    </span>
+  ) : (
+    <a className="link" href={repo} target="_blank">Github page</a>
+  );
+}
+
+function Card({jsonData}) {
+  // Construct images for carousel
+  const keys = Object.keys(jsonData.images);
+  const listItems = keys.map((index) => { 
+    return (
+      <CarouselImage 
+        key={index} 
+        index={index} 
+        id={`${jsonData.id}${index}`} 
+        src={jsonData.images[index].src} 
+        alt={jsonData.images[index].alt} 
+      />
+    )
+  });
+  // Construct scroll-markers
+  const buttons = keys.map((index) => {
+    return (
+      <a key={index} className={`scroll-marker ${jsonData.id} ${index == 0 ? "active" : ""}`} id={`${jsonData.id}${index}marker`}></a>
+    )
+  });
+  // Construct tech stack
+  const stack = jsonData.stack.map((item) => {
+    return (
+      <p key={item} className="stackItem">{item}</p>
+    )
+  });
+  // Add scrollIntoView to scroll-markers
+  useEffect(() => {
+    const images = document.querySelectorAll(`#${jsonData.id} li`)
+    const buttons = document.querySelectorAll(`#${jsonData.id} .scroll-marker-container a`)
+
+    for (let i = 0; i < images.length; i++) {
+      buttons[i].onclick = () => {
+        images[i].scrollIntoView({block: 'nearest'});
+      }
+    }
+  }, [])
+  //
+  function moveLeft() {
+    
+  }
+
+  return (
+    <>
+      <div className="card" id={jsonData.id}>
+        <div className="gallery-container">
+          <div className="carousel-container">
+            <ul className="carousel"> 
+              {listItems}
+            </ul>
+            <button className="scroll-button left" id={`${jsonData.id}lbutton`} onClick={()=>{}}><Arrow /></button>
+            <button className="scroll-button right" id={`${jsonData.id}rbutton`}><Arrow /></button>
+          </div>
+          <div className="scroll-marker-container">
+            {buttons}
+          </div>
+        </div>
+        <Heading h1={jsonData.name} su={jsonData.subtext} />
+        <ProjectLinks site={jsonData.site_url} repo={jsonData.repo_url}/>
+        <p>{jsonData.description}</p>
+        <div className="stack-container">
+          <p className="stack">Tech stack:</p>
+          <div>
+            {stack}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import '../styles/projects.css'
 import '../styles/index.css'
 
 import Heading from './Heading.jsx'
+import Card from './Card.jsx'
 
 import {useEffect} from 'react'
 import galaData from '../projects/galatimer.json' with {type: 'json'}
@@ -9,105 +10,9 @@ import coinData from '../projects/coinanalyzer.json' with {type: 'json'}
 import rotypeData from '../projects/rotype.json' with {type: 'json'}
 import templateData from '../projects/card-template.json' with {type: 'json'}
 
-function Arrow() {
-  return (
-    <>
-      <svg className="arrow"
-        viewBox="0 0 1.2001499 1.2001499"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:svg="http://www.w3.org/2000/svg">
-        <defs
-          id="defs1" />
-        <path
-          style={{'fill':'none','stroke':'currentColor','strokeWidth':'0.264583','strokeLinecap':'round','strokeLinejoin':'round','strokeDasharray':'none'}}
-          d="m 0.13235301,0.73432379 0.4677219,-0.4677219 0.46772199,0.4677219"
-          id="path1" />
-      </svg>
-    </>
-  )
-}
-function Card({jsonData}) {
-  // Construct images for carousel
-  const keys = Object.keys(jsonData.images);
-  const listItems = keys.map((img) => { 
-    return (
-      <li key={img} className="image" id={`${jsonData.id}${img}`}>
-        <img src={jsonData.images[img].src} alt={jsonData.images[img].alt} />
-      </li>
-    )
-  });
-  // Construct scroll-markers
-  const buttons = keys.map((img) => {
-    return (
-      <a key={img} className={`scroll-marker ${jsonData.id} ${img == 0 ? "active" : ""}`} id={`${jsonData.id}${img}marker`}></a>
-    )
-  });
-  // Construct 
-  // Construct links
-  const links = jsonData.site_url ? (
-    <span className="card-link-container">
-      <a className="link" href={jsonData.site_url} target="_blank">Website</a>
-      <span className="subtext" aria-hidden="true">|</span>
-      <a className="link" href={jsonData.repo_url} target="_blank">Github page</a>
-    </span>
-  ) : (
-    <a className="link" href={jsonData.repo_url} target="_blank">Github page</a>
-  );
-  // Construct tech stack
-  const stack = jsonData.stack.map((item) => {
-    return (
-      <p key={item} className="stackItem">{item}</p>
-    )
-  });
-  // Add scrollIntoView to scroll-markers
-  useEffect(() => {
-    const images = document.querySelectorAll(`#${jsonData.id} li`)
-    const buttons = document.querySelectorAll(`#${jsonData.id} .scroll-marker-container a`)
-
-    for (let i = 0; i < images.length; i++) {
-      buttons[i].onclick = () => {
-        images[i].scrollIntoView({block: 'nearest'});
-        
-      }
-    }
-  }, [])
-  //
-  function moveLeft() {
-    
-  }
-
-  return (
-    <>
-      <div className="card" id={jsonData.id}>
-        <div className="gallery-container">
-          <div className="carousel-container">
-            <ul className="carousel"> 
-              {listItems}
-            </ul>
-            <button className="scroll-button left"><Arrow /></button>
-            <button className="scroll-button right"><Arrow /></button>
-          </div>
-          <div className="scroll-marker-container">
-            {buttons}
-          </div>
-        </div>
-        <Heading h1={jsonData.name} su={jsonData.subtext} />
-        {links}
-        <p>{jsonData.description}</p>
-        <div className="stack-container">
-          <p className="stack">Tech stack:</p>
-          <div>
-            {stack}
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
-
 function Projects() {
   useEffect(() => {
+    // Reveal cards for the first time
     const cardObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.intersectionRatio > 0) {
@@ -116,10 +21,10 @@ function Projects() {
         }
       });
     }, {threshold: [0]});
+    // Update carousel to focused image
     const imageObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.intersectionRatio < 0.75) return;
-        console.log(entry.intersectionRatio)
         const id = entry.target.id.substring(0, entry.target.id.length-1);
         document.querySelectorAll(`a.${id}.active`).forEach((e) => {
           e.classList.remove("active");
